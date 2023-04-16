5472944b4f4051c2c159e23fb2734797413ede0a
AMAN-MAHTO <amahto848@gmail.com>
2023-04-16 21:46:16 +0530

    overworld csv file added

---
+++ level/tiles_set/level_images.tsx
@@ -0,0 +1,22 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.10" tiledversion="1.10.1" name="level_images" tilewidth="200" tileheight="200" tilecount="6" columns="0">
+ <grid orientation="orthogonal" width="1" height="1"/>
+ <tile id="0">
+  <image width="200" height="200" source="../../graphics/overworld/0/1.png"/>
+ </tile>
+ <tile id="1">
+  <image width="200" height="200" source="../../graphics/overworld/1/1.png"/>
+ </tile>
+ <tile id="2">
+  <image width="200" height="200" source="../../graphics/overworld/2/1.png"/>
+ </tile>
+ <tile id="3">
+  <image width="200" height="200" source="../../graphics/overworld/3/1.png"/>
+ </tile>
+ <tile id="4">
+  <image width="200" height="200" source="../../graphics/overworld/4/1.png"/>
+ </tile>
+ <tile id="5">
+  <image width="200" height="200" source="../../graphics/overworld/5/1.png"/>
+ </tile>
+</tileset>
